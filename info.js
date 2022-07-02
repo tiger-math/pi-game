1ef6b992dbd278c73f523c7f67d31bfb3fb773f5
six2-110 <108448719+six2-110@users.noreply.github.com>
2022-07-02 23:07:18 +0900

    Add files via upload

--- info.js
+++ info.js
@@ -11,13 +11,20 @@
 
 const site = [
     {
+        name: 'number-guessing-game',
+        tag: [0],
+        link: 'number-guessing-game/index.html',
+		img: '数字.webp',
+		date: '2022.07.02',
+        txt: '0<n≦100を満たす自然数nを6回のチャンスで当てるゲーム'
+    },{
         name: 'click-game',
         tag: [0],
         link: 'click-game/index.html',
 		img: 'カーソル.jpeg',
 		date: '2022.07.01',
-        txt: 'クリックしていくだけのゲームです'
-    },
+        txt: 'クリックしていくだけのゲーム'
+    }
 ]
 
 const tag = [
@@ -50,8 +57,63 @@ function apC() {
 	text_box.appendChild(new_elm);
 }
 
+function inB() {
+    text_box.insertBefore(new_elm,text_box.firstChild);
+}
+
 const loc = {
     href: location.href,
     hash: location.hash.replace('#',''),
     search: Number(location.search.replace('?','')),
 }
+
+{/* <div id="header">
+        <a href="index.html">
+            <img src="img/favicon.jpeg" alt="title" height="100" align="left">
+        </a>
+        <span id="site_title">pi-game</span>
+    </div> */}
+
+// header
+var a = loc.href.split('/');
+var b = a.length - a.indexOf('pi-game') - 2;
+var c = '';
+for (i=0; i<b; i++) {
+    c += '../';
+}
+
+// div#header
+text_box = $('body')[0];
+create('div');
+new_elm.id = 'header';
+inB();
+
+// a:index.html
+text_box = $('#header');
+create('a')
+new_elm.id = 'header_a';
+new_elm.href = c + 'index.html';
+apC();
+
+// img:favicon.jpeg
+text_box = $('#header_a');
+create('img');
+new_elm.src = c + 'img/favicon.jpeg';
+new_elm.alt = 'title';
+new_elm.height = '100';
+new_elm.align = 'left';
+apC();
+
+// span#site_title
+text_box = $('#header');
+create('span');
+new_elm.id = 'site_title';
+new_elm.innerText = 'pi-game';
+apC();
+
+//link:page.css
+text_box = $('head')[0];
+create('link');
+new_elm.rel = 'stylesheet';
+new_elm.href = c + 'page.css';
+apC();
