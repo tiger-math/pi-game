c8371e02ce3f1492166b3c2c3df0a801258e4d1c
six2-110 <108448719+six2-110@users.noreply.github.com>
2022-07-03 21:53:05 +0900

    Update info.js

--- info.js
+++ info.js
@@ -12,14 +12,14 @@
 const site = [
     {
         name: 'number-guessing-game',
-        tag: [0,2,3,4],
+        tag: [0,3,4,5],
         link: 'number-guessing-game/index.html',
 		img: '数字.webp',
 		date: '2022.07.02',
         txt: '0<n≦100を満たす自然数nを6回のチャンスで当てるゲーム'
     },{
         name: 'click-game',
-        tag: [0,4],
+        tag: [0,5],
         link: 'click-game/index.html',
 		img: 'カーソル.jpeg',
 		date: '2022.07.01',
