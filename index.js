4060cb943d1ca96de89c48348c76caec5259d697
six2-110 <108448719+six2-110@users.noreply.github.com>
2022-07-05 06:49:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,104 +1,147 @@
-//---------------page---------------
-for (i=0; i<site.length; i++) {
-	if (site_name == 'index' || site[i].tag.includes(loc.search)) {
-	 //div.page
-	text_box = $('#contents');
-    create('div');
-    new_elm.className = 'page';
-    new_elm.id = 'page' + i;
-    apC();
-	
-	 //a.page_tag
-    for (i2=0; i2<site[i].tag.length; i2++) {
-		text_box = $('#page'+i);
+/*
+{
+        name: '',
+        tag: [],
+        link: '',
+		img: '',
+		date: '',
+        txt: ''
+    },
+*/
 
-        create('a');
-        new_elm.href = 'tag.html?' + site[i].tag[i2];
-        new_elm.innerText = '#' + tag[site[i].tag[i2]];
-        new_elm.className = 'page_tag';
-        apC();
-
-        create('span');
-        new_elm.innerText = ' ';
-        apC();
+const site = [
+    {
+        name: 'number-guessing-game',
+        tag: [0,3,4,5],
+        link: 'number-guessing-game/index.html',
+		img: 'number-guessing.webp',
+		date: '2022.07.02',
+        txt: '0<n≦100を満たす自然数nを6回のチャンスで当てるゲーム'
+    },{
+        name: 'click-game',
+        tag: [0,5],
+        link: 'click-game/index.html',
+		img: 'click-game.jpeg',
+		date: '2022.07.01',
+        txt: 'クリックしていくだけのゲーム'
     }
-	
-	 //br
-	create('br');
-	apC();
-	
-	//img.page_img
-	create('img');
-	new_elm.className = 'page_img';
-	new_elm.src = 'img/' + site[i].img;
-	new_elm.alt = 'img';
-	apC();
-	
-	//a.page_name
-	create('a');
-	new_elm.className = 'page_name';
-	new_elm.innerText = site[i].name;
-	new_elm.href = site[i].link;
-	apC();
-	
-	//span.page_date
-	create('span');
-	new_elm.className = 'page_date';
-	new_elm.innerText = '最終更新日. ' + site[i].date;
-	apC();
-	
-	//hr.page_hr
-	create('hr');
-	new_elm.className = 'page_hr';
-	apC();
-	
-	//div.page_txt
-	create('div');
-	new_elm.className = 'page_txt';
-	new_elm.innerText = site[i].txt;
-	apC();
-	}
+]
+
+const tag = [
+    'ゲーム',
+    'ツール',
+    '記事',
+    '頭脳系',
+    'パズル',
+    '暇つぶし'
+];
+
+const header_menu = [
+    {
+        txt: 'フィードバック',
+        link: 'feedback'
+    }
+]
+
+function $(dom) {
+    if (dom.startsWith('.')) {
+        return document.getElementsByClassName(dom.replace('.',''));
+    } else {if (dom.startsWith('#')) {
+        return document.getElementById(dom.replace('#',''));
+    } else {
+        return document.getElementsByTagName(dom);
+    }}
 }
 
-//---------------tag---------------
-let numbers_tag = [];
-for (i=0; i<tag.length; i++) {
-	// tr#tag_tr+i
-	text_box = $('#tag_table')
-	create('tr');
-	new_elm.id = 'tag_tr' + i;
-	apC();
-
-	text_box = $('#tag_tr'+i);
-	create('td');
-	new_elm.id = 'tag_name' + i;
-	apC();
-
-	text_box = $('#tag_name'+i);
-	create('a');
-	new_elm.href = 'tag.html?' + i;
-	new_elm.innerText = '#' + tag[i];
-	apC();
-
-	text_box = $('#tag_tr'+i);
-	create('td');
-	new_elm.id = 'numbers_tag' + i;
-	apC();
-
-
-	numbers_tag.push(0);
+const $doc = document;
+let site_name = location.href.split('/').pop().replace('.html','');
+if (site_name == '') {
+    site_name = 'index';
 }
 
-for (i=0; i<site.length; i++) {
-	for (i2=0; i2<site[i].tag.length; i2++) {
-		numbers_tag[site[i].tag[i2]]++;
-	}
+var new_elm, text_box;
+function create(tag) {
+    new_elm = $doc.createElement(tag);
 }
 
-for (i=0; i<tag.length; i++) {
-	$('#numbers_tag'+i).innerText = numbers_tag[i];
+function apC() {
+	text_box.appendChild(new_elm);
 }
 
+function inB() {
+    text_box.insertBefore(new_elm,text_box.firstChild);
+}
+
+const loc = {
+    href: location.href,
+    hash: location.hash.replace('#',''),
+    search: Number(location.search.replace('?','')),
+}
+
+// header
+var a = loc.href.split('/');
+var b = a.length - a.indexOf('pi-game') - 2;
+var c = '';
+for (i=0; i<b; i++) {
+    c += '../';
+}
+
+// header
+text_box = $('body')[0];
+create('header');
+inB();
+
+// a:index.html
+text_box = $('header')[0];
+create('a')
+new_elm.id = 'header_a';
+new_elm.href = c + 'index.html';
+apC();
+
+// img:favicon.jpeg
+text_box = $('#header_a');
+create('img');
+new_elm.src = c + 'img/favicon.jpeg';
+new_elm.alt = 'title';
+new_elm.height = '100';
+new_elm.align = 'left';
+apC();
+
+// h1#site_title
+text_box = $('header')[0];
+create('h1');
+new_elm.id = 'site_title';
+new_elm.innerText = 'pi-game';
+apC();
+
+// ul#header_menu_group
+create('ul');
+new_elm.id = 'header_menu_group';
+apC();
+
+text_box = $('#header_menu_group');
+for (i=0; i<header_menu.length; i++) {
+    // li.header_menu_item
+    create('li');
+    new_elm.className = 'header_menu_item';
+    apC();
+
+    // a
+    text_box = $('.header_menu_item')[$('.header_menu_item').length - 1]
+    create('a');
+    new_elm.href = c + header_menu[i].link + '.html';
+    new_elm.innerText = header_menu[i].txt;
+    apC();
+}
+
+//link:page.css
+text_box = $('head')[0];
+create('link');
+new_elm.rel = 'stylesheet';
+new_elm.href = c + 'page.css';
+apC();
+
+// title
 if (site_name != 'index') {
-	$('#contents_h1').innerText = '#' + tag[loc.search];
+    $('title')[0].innerText += ' |pi-game';
 }
